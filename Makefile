534f56e53ec28d53d02ba6fb3666efcc663cffa4
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-10-31 23:03:16 -0500

    Add RS_DIR variable to scripts,  improve kahoot-names

--- Makefile
+++ Makefile
@@ -40,6 +40,7 @@ $(scriptsbuilds): $(OUTDIR)/%: $(addsuffix .sh,scripts/%) \
 		$$(addprefix $$(OUTDIR)/,$$(shell $(get_deps)))
 	echo '#!/usr/bin/env bash' > $@
 	echo 'PATH="$$PATH":"$(realpath $(OUTDIR))"' >> $@
+	echo 'RS_DIR="$(realpath .)"' >> $@
 	cat $< >> $@
 	chmod +x $@
 
--- scripts/kahoot-names.sh
+++ scripts/kahoot-names.sh
@@ -1,13 +1,28 @@
+show_help() {
+  echo 'Usage: names (flood|rand) <game pin> <name list>'
+}
 case $1 in
   --deps)
     echo "kahoot-flood kahoot-rand"
   ;;
   flood|rand)
-    
-    kahoot-$1 "$2" "kahoot-names/$3"
+    list="$RS_DIR/kahoot-names/$3"
+    if [[ ! -f $list ]]; then
+      echo "Names list '$3' not found"
+      exit 1
+    fi
+    kahoot-$1 "$2" "$list"
+  ;;
+  --help)
+    show_help
+  ;;
+  "")
+    show_help
+    exit 1
   ;;
   *)
     echo "Invalid option"
+    show_help
     exit 1
   ;;
 esac
