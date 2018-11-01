c2653ec1e4dd90cda6fe57d96c9907054ed09d2a
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-10-31 23:07:05 -0500

    Add .PHONY for some recipes in generic.mk

--- generic.mk
+++ generic.mk
@@ -2,11 +2,14 @@ define __defvars =
  allbins += $$($(1)bins)
  $(1)builds = $$(patsubst %,$(OUTDIR)/%,$$($(1)bins))
  allbuilds += $$($(1)builds)
+ .PHONY: $(1) install-$(1) uninstall-$(1)
  $(1): $$($(1)builds)
  install-$(1): $$(addprefix install-,$$($(1)bins))
  uninstall-$(1): $$(addprefix uninstall-,$$($(1)bins))
 endef
 $(foreach target,$(targets),$(eval $(call __defvars,$(target))))
+
+.PHONY: install uninstall $(addprefix install-,$(allbins)) $(addprefix uninstall-,$(allbins))
 
 install: $(addprefix install-,$(targets))
 
