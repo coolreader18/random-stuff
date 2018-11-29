774d11779e48b56922bc23c3b94f2b27714f378c
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-11-29 16:57:08 +0000

    Fix coder setup

--- .setup/coder.sh
+++ .setup/coder.sh
@@ -2,10 +2,12 @@ apt install make bison
 
 curl -sSL https://github.com/moovweb/gvm/raw/master/binscripts/gvm-installer | bash
 
+. ~/.gvm/scripts/gvm
+
 if [[ ! $GO_VERSION ]]; then
 	GO_VERSION=go1.10
 fi
 
 gvm install "$GO_VERSION" -B
 
-update-alternatives --install /usr/bin/go go ~/.gvm/gos/go1.10/ 1
+update-alternatives --install /usr/bin/go go ~/.gvm/gos/$GO_VERSION/bin/go 1
